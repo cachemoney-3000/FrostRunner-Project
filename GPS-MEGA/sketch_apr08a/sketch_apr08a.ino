0e5b28f08e1d25ed9672f15cde87a824525204a5
joshuaRSU <joshua@runsignup.com>
2023-06-09 23:59:11 -0400

    update

--- GPS-MEGA/sketch_apr08a/sketch_apr08a.ino
+++ GPS-MEGA/sketch_apr08a/sketch_apr08a.ino
@@ -23,8 +23,8 @@ int bluetoothReadFlag = 0;
 
 //******************************************************************************************************     
 String location="";
-double targetLatitude = 28.59108000;
-double targetLongitude = -81.46820800;
+double targetLatitude; // = 28.59108000;
+double targetLongitude; // = -81.46820800;
 //******************************************************************************************************
 // Compass Variables & Setup
 QMC5883LCompass compass;
@@ -51,7 +51,77 @@ void setup()
 
 void loop()
 {
-  return 0;
+  while (Serial2.available() > 0){
+    char receivedChar = Serial2.read();
+
+    if (receivedChar == '/') {
+      if (location != "") {
+        // Split the location string into longitude and latitude
+        int separatorIndex = location.indexOf('\n');
+        if (separatorIndex != -1) {
+          String longitudeString = location.substring(0, separatorIndex);
+          String latitudeString = location.substring(separatorIndex + 1);
+
+          // Convert latitude and longitude to float values
+          targetLatitude = latitudeString.toDouble();
+          targetLongitude = longitudeString.toDouble();
+        }
+
+        Serial.println("Target Longitude: " + String(targetLongitude, 8));
+        Serial.println("Target Latitude: " + String(targetLatitude, 8));
+
+        location = ""; // Reset the location buffer
+        
+        break; // End transmission
+      }
+    }
+    else {
+      location += receivedChar;
+    }
+  }
+
+  Location phoneLoc;
+  phoneLoc.latitude = targetLatitude.toDouble();
+  phoneLoc.longitude = targetLongitude.toDouble();
+
+  driveTo(phoneLoc, GPS_WAYPOINT_TIMEOUT);
+}
+
+void driveTo(struct GeoLoc &loc, int timeout) {
+  Serial1.listen();
+  GeoLoc robotLoc = getGPS();
+  Serial2.listen();
+
+  if (robotLoc.lat != 0 && robotLoc.lon != 0) {
+    float distance = 0;
+    //Start move loop here
+    do {
+      Serial1.listen();
+      robotLoc = getGPS();
+      Serial2.listen();
+      
+      // TODO geoDistance
+      distance = geoDistance(robotLoc, loc);
+      // TODO geoBearing, heoHeading
+      float bearing = geoBearing(robotLoc, loc) - geoHeading();
+      
+      Serial.print("Distance: ");
+      Serial.println(distance);
+    
+      Serial.print("Bearing: ");
+      Serial.println(geoBearing(robotLoc, loc));
+
+      Serial.print("Heading: ");
+      Serial.println(geoHeading());
+      
+      // TODO drive
+      drive(distance, bearing);
+      timeout -= 1;
+    } while (distance > 1.0 && timeout > 0);
+
+    // TODO stop
+    stop();
+  }
 }
 
 
